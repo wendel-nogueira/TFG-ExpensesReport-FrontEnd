acd476adc3ca244912877c5ca0e4ca4cdb0816ee
Wendel Nogueira <wnogueira197@gmail.com>
2024-02-10 15:47:46 -0300

    feat(application): add initial code

--- apps/expensesreport/src/app/app.component.ts
+++ apps/expensesreport/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'expensesreport-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'expensesreport';
+  title = 'Expenses Report';
 }
