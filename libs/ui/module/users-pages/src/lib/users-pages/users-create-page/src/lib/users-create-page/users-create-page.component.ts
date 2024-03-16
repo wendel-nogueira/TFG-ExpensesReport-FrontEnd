c3e9b04cc490b2921f230a4fb581bfaf8e1e80bf
Wendel Nogueira <wnogueira197@gmail.com>
2024-03-16 20:52:43 -0300

    feat(create-user): add page content

--- libs/ui/module/users-pages/src/lib/users-pages/users-create-page/src/lib/users-create-page/users-create-page.component.ts
+++ libs/ui/module/users-pages/src/lib/users-pages/users-create-page/src/lib/users-create-page/users-create-page.component.ts
@@ -1,11 +1,88 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  PageContentComponent,
+  InputTextComponent,
+  SelectComponent,
+  FormGroupComponent,
+  LabelComponent,
+  ButtonComponent,
+} from '@expensesreport/ui';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'expensesreport-users-create-page',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './users-create-page.component.html',
   styleUrl: './users-create-page.component.css',
+  imports: [
+    CommonModule,
+    PageContentComponent,
+    InputTextComponent,
+    SelectComponent,
+    FormGroupComponent,
+    LabelComponent,
+    ButtonComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class UsersCreatePageComponent {}
+export class UsersCreatePageComponent {
+  loading = false;
+  disabled = false;
+  roles = roles;
+
+  userFormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    role: new FormControl('', [Validators.required]),
+  });
+  emailErrors = {
+    email: 'Email is invalid',
+    exists: 'Email already exists',
+  };
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.userFormGroup.invalid) {
+      Object.keys(this.userFormGroup.controls).forEach((controlName) => {
+        const control = this.userFormGroup.get(controlName);
+
+        if (control) {
+          control.markAsTouched();
+          control.updateValueAndValidity();
+        }
+      });
+
+      return;
+    }
+
+    console.log(this.userFormGroup.value);
+  }
+
+  onBack() {
+    this.router.navigate(['/users']);
+  }
+}
+
+const roles = [
+  {
+    label: 'Field Staff',
+    value: 'fieldStaff',
+  },
+  {
+    label: 'Manager',
+    value: 'manager',
+  },
+  {
+    label: 'Accountant',
+    value: 'accountant',
+  },
+];
