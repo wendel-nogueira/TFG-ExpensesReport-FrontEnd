ad1c5afd01fd3328db0e41742ffc4c62485e0430
Wendel Nogueira <wnogueira197@gmail.com>
2024-03-18 04:52:36 -0300

    feat(list-departments): add page content

--- libs/ui/module/departments-pages/src/lib/departments-pages/departments-list-page/src/lib/departments-list-page/departments-list-page.component.ts
+++ libs/ui/module/departments-pages/src/lib/departments-pages/departments-list-page/src/lib/departments-list-page/departments-list-page.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent, TableColumn } from '@expensesreport/ui';
+import { OverlayPanelModule, OverlayPanel } from 'primeng/overlaypanel';
+import { ViewComponent } from '@expensesreport/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'expensesreport-departments-list-page',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './departments-list-page.component.html',
   styleUrl: './departments-list-page.component.css',
+  imports: [
+    CommonModule,
+    OverlayPanelModule,
+    RouterModule,
+    TableComponent,
+    ViewComponent,
+  ],
 })
-export class DepartmentsListPageComponent {}
+export class DepartmentsListPageComponent {
+  columns: TableColumn[] = [];
+  departments: any[] = [];
+  loading = false;
+
+  dataClicked: any | null = null;
+
+  onclickMore(event: { event: PointerEvent; data: any }, op: OverlayPanel) {
+    this.dataClicked = event.data;
+
+    op.toggle(event.event);
+  }
+}
