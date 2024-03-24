9e84cff149d6b7b9d4d92bbdea94c021d2916f18
Wendel Nogueira <wnogueira197@gmail.com>
2024-03-23 21:18:08 -0300

    feat(edit-projects): add page content

--- libs/ui/module/projects-pages/src/lib/projects-pages/projects-edit-page/src/lib/projects-edit-page/projects-edit-page.component.ts
+++ libs/ui/module/projects-pages/src/lib/projects-pages/projects-edit-page/src/lib/projects-edit-page/projects-edit-page.component.ts
@@ -1,11 +1,102 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  PageContentComponent,
+  InputTextComponent,
+  SelectComponent,
+  FormGroupComponent,
+  LabelComponent,
+  ButtonComponent,
+  TextAreaComponent,
+} from '@expensesreport/ui';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'expensesreport-projects-edit-page',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './projects-edit-page.component.html',
   styleUrl: './projects-edit-page.component.css',
+  imports: [
+    CommonModule,
+    PageContentComponent,
+    InputTextComponent,
+    SelectComponent,
+    FormGroupComponent,
+    LabelComponent,
+    ButtonComponent,
+    TextAreaComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class ProjectsEditPageComponent {}
+export class ProjectsEditPageComponent {
+  loading = false;
+  disabled = false;
+  isDeactivated = false;
+
+  projectFormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    code: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(10),
+    ]),
+    description: new FormControl('', [Validators.maxLength(100)]),
+  });
+  nameErrors = {
+    required: 'Name is required',
+    minlength: 'Name must be at least 3 characters',
+    maxlength: 'Name must be at most 50 characters',
+  };
+  codeErrors = {
+    required: 'Code is required',
+    minlength: 'Code must be at least 2 characters',
+    maxlength: 'Code must be at most 10 characters',
+    exists: 'Code already exists',
+  };
+  descriptionErrors = {
+    maxlength: 'Description must be at most 100 characters',
+  };
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.projectFormGroup.invalid) {
+      Object.keys(this.projectFormGroup.controls).forEach((controlName) => {
+        const control = this.projectFormGroup.get(controlName);
+
+        if (control) {
+          control.markAsTouched();
+          control.updateValueAndValidity();
+        }
+      });
+
+      return;
+    }
+
+    console.log(this.projectFormGroup.value);
+  }
+
+  onBack() {
+    this.router.navigate(['/projects']);
+  }
+
+  onDeactivate() {
+    console.log('Deactivate');
+  }
+
+  onActivate() {
+    console.log('Activate');
+  }
+}
