ade7c52c08cca9e8d6ac903575240c2e0408f36c
Wendel Nogueira <wnogueira197@gmail.com>
2024-04-11 11:27:14 -0300

    feat(list-expense-reports): add page content

--- libs/ui/module/expense-reports-pages/src/lib/expense-reports-pages/expense-reports-list-page/src/lib/expense-reports-list-page/expense-reports-list-page.component.ts
+++ libs/ui/module/expense-reports-pages/src/lib/expense-reports-pages/expense-reports-list-page/src/lib/expense-reports-list-page/expense-reports-list-page.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableComponent, TableColumn } from '@expensesreport/ui';
+import { OverlayPanelModule, OverlayPanel } from 'primeng/overlaypanel';
+import { ViewComponent } from '@expensesreport/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'expensesreport-expense-reports-list-page',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './expense-reports-list-page.component.html',
   styleUrl: './expense-reports-list-page.component.css',
+  imports: [
+    CommonModule,
+    OverlayPanelModule,
+    RouterModule,
+    TableComponent,
+    ViewComponent,
+  ],
 })
-export class ExpenseReportsListPageComponent {}
+export class ExpenseReportsListPageComponent {
+  columns: TableColumn[] = [];
+  reports: any[] = [];
+  loading = false;
+
+  dataClicked: any | null = null;
+
+  onclickMore(event: { event: PointerEvent; data: any }, op: OverlayPanel) {
+    this.dataClicked = event.data;
+
+    op.toggle(event.event);
+  }
+}
